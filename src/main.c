2de1e1e4389c3931667298a6f398746a83eb2b58
Motsoaledi Neo Tshoana <mntshoana@outlook.com>
2021-02-05 14:10:41 +0200

    Turns off echo flag for when having input.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h> // to turn of echo mode
+
 
 int main () {
-    printf("Hello World\n");
+    // first turn of echo mode
+    struct termios rawFlags;
+    tcgetattr(STDIN_FILENO, &rawFlags);
+    rawFlags.c_lflag &= ~(ECHO); // echo off
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawFlags);
+    
+    printf("Welcome.Feel free to type. Type q to quit\n");
+    char c;
+    // still in cononical mode
+    while (read(STDIN_FILENO, &c, 1) == 1) {
+        if (c == 'q')
+            break;
+    }
     return 0;
 }
